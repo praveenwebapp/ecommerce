ab702ff8195388a927c689b85025a9245df40648
PRINCE <praveenmaddela@PRINCEs-MacBook-Pro.local>
2024-12-06 01:34:22 +0530

    config changes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,10 +1,13 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  server: {port : 5174},
   build: {
     rollupOptions: {
       external: ['react-router-dom'],
     },
   },
-});
+})
