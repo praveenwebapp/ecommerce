5a2a42e3e653af1e3e193a0872025907a7751ddc
PRINCE <praveenmaddela@PRINCEs-MacBook-Pro.local>
2024-12-06 01:39:33 +0530

    config changes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {port : 5174},
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
+  plugins: [react()]
+
 })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {port:5173},
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
+  plugins: [react()]
 })
