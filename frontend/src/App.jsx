9ecb2ab76c42f031f2daa5ad75a6987c8618e17e
PRINCE <praveenmaddela@PRINCEs-MacBook-Pro.local>
2024-12-08 02:21:54 +0530

    changing doms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router'
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
