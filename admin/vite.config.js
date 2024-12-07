f4bc39eb42bd2c2040be8bb5bc0c33fe404c3dae
PRINCE <praveenmaddela@PRINCEs-MacBook-Pro.local>
2024-12-08 02:01:18 +0530

    changes for resolve dom issue

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {port : 5174},
   rollupOptions: {
-    external: ['react-router']
+    external: process.env.VITE_BACKEND_URL === 'https://ecommerce-backend-29qk.onrender.com"' ? ['react-router-dom'] : []
   }
 })
