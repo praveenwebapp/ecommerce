2da8ae7719eb92ca8f164ee458208c322826fe93
PRINCE <praveenmaddela@PRINCEs-MacBook-Pro.local>
2024-12-08 02:30:28 +0530

    changing versionas

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {port : 5174},
-  rollupOptions: {
-    external: process.env.VITE_BACKEND_URL === 'https://ecommerce-backend-29qk.onrender.com"' ? ['react-router-dom'] : []
-  }
-})
+  plugins: [react()]
+});
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {port:5173},
-  rollupOptions: {
-    external: ['react-router']
-  }
-})
+  plugins: [react()]
+});
