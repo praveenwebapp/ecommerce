33c4140ea59552e1660a096cc31c6fedf60d25f6
PRINCE <praveenmaddela@PRINCEs-MacBook-Pro.local>
2024-12-08 02:50:20 +0530

    changing versionas

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  resolve: {
+    alias: {
+      'react-router-dom': 'react-router-dom/umd/react-router-dom.min.js',
+    },
+  },
 });
