6c5aea096ccce4dfac677507f5d257e008774aff
PRINCE <praveenmaddela@PRINCEs-MacBook-Pro.local>
2024-12-12 21:33:22 +0530

    testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,7 @@ export default defineConfig({
   server: {port:5173},
   build: {
     rollupOptions: {
-      external: ['react-router-dom','react-toastify','ReactToastify.css'],
+      external: ['react-router-dom','react-toastify','ReactToastify'],
     },
-  },
-  optimizeDeps: {
-    include: ['react-toastify'],
-  },
+  }
 })
